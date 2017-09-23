0726c21b5983afe5facef29bff0aa59a7355bdf3
Danimuheim <31336903+Danimuheim@users.noreply.github.com>
2017-09-23 16:48:08 +0200

    Initial commit

---
+++ 2FactorAuth/2FactorDotNet-master/2FactAuthDotNet/Login.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2FactAuthDotNet
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text == "daniele.muheim@gmail.com" && txtPassword.Text == "password")
+            {
+                Response.Redirect("Auth.aspx?userid=1");
+            }
+        }
+    }
+}
---
+++ 2FactorAuth/2FactorDotNet-master/2FactAuthDotNet/Validate.aspx.cs
@@ -0,0 +1,52 @@
+﻿using Google.Authenticator;
+using System;
+using System.Configuration;
+
+namespace _2FactAuthDotNet
+{
+    public partial class Validate : System.Web.UI.Page
+    {
+        /// <summary>
+        /// Generate Key
+        /// </summary>
+        public string Key
+        {
+            get
+            {
+                string strQrKey = string.Empty;
+                if (ConfigurationManager.AppSettings.Get("QRKey") != null)
+                {
+                    strQrKey = ConfigurationManager.AppSettings.Get("QRKey").ToString();
+                }
+                return strQrKey;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Verify();
+        }
+        /// <summary>
+        /// Verify key
+        /// </summary>
+        public void Verify()
+        {
+            string user_enter = txtQR.Text;
+            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+            string strCurrentPin = tfa.GetCurrentPIN(Key + Session["user"].ToString());
+            if (strCurrentPin.Equals(user_enter))
+            {
+                lblResponse.Text = "Success";
+            }
+            else
+            {
+                lblResponse.Text = "Failure current key used is "+ strCurrentPin;
+            }
+        }
+    }
+}
